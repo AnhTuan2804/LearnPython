d5c58a8af68656baf8b22d9b35992f2271b834e7
anhtuan2804 <leanhtuan.it14@gmail.com>
2018-09-17 23:23:49 +0700

    first commit: python basic

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LearnPython
+Selfstudy Python language
---
+++ learn.py
@@ -0,0 +1,51 @@
+import datetime
+
+# item = [12, 2,6,4,9]
+
+# print(item)
+
+# print(sum(item))
+
+# item.sort()
+# print(item)
+
+# def sumItem(list):
+#     total = 0
+#     for subitem in list:
+#         total +=subitem
+#     return total, total
+
+# print(sumItem(item)[0])
+
+class person():
+    name = 'Dog'
+    age = 20
+    color = 'blue'
+    eye = 'green'
+
+    def getColor(self):
+        return self.color
+
+    def getAge(abc):
+        return abc.age
+
+class Teo(person):
+    def getEye(self):
+        return self.eye
+
+# boy = person()
+# print(boy.getAge())
+# print(boy.getColor())
+
+# print('end')
+
+# TeoEm = Teo()
+# print(TeoEm.getColor())
+# print(TeoEm.getEye())
+
+print(datetime.date.today())
+print(datetime.date.today().year)
+
+arr = ['aca', 'aaa']
+
+
